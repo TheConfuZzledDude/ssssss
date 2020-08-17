789c90a958aedd37b7c0216945e7464dd8e9a507
TheConfuZzledDude <theconfuzzleddude@gmail.com>
2020-08-18 00:38:05 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Welcome to the Supreme Sorcerous Sorcerer's Sircle of Saucy Sorcery (Don't tell anyone)
+    </div>
+  );
+}
+
+export default App;
